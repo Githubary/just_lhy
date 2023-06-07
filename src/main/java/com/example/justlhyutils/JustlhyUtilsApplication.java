a960d3faa8cd98735462db8162dbf6f03a84af46
liuhuayang <969204544@qq.com>
2023-06-07 16:48:09 +0800

    update

---
+++ src/main/java/com/example/justlhyutils/JustlhyUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.justlhyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustlhyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustlhyUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/justlhyutils/JustlhyUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.justlhyutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustlhyUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
