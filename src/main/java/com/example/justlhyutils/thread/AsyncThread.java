37650c153b15eec10372bdc130eb1ad02f4f124d
liuhuayang <969204544@qq.com>
2023-06-27 20:05:03 +0800

    update:批量读取文件第一步

--- src/main/java/com/example/justlhyutils/thread/AsyncThread.java
+++ src/main/java/com/example/justlhyutils/thread/AsyncThread.java
@@ -1,13 +1,36 @@
 package com.example.justlhyutils.thread;
 
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
 /**
  * description:
  *
  * @author liuhuayang
  * date: 2023/6/26 10:14
  */
+@Component
 public class AsyncThread {
 
+    private final ThreadPoolTaskExecutor asyncTaskExecutor;
 
+    public AsyncThread(ThreadPoolTaskExecutor asyncTaskExecutor) {
+        this.asyncTaskExecutor = asyncTaskExecutor;
+    }
+
+    public void doSomethingAsync() {
+        for (int i = 0; i < 10; i++) {
+            asyncTaskExecutor.submit(new MyAsyncTask());
+        }
+    }
+
+    static class MyAsyncTask implements Runnable{
+        @Override
+        public void run() {
+            File file = new File("classpath:file/batchReadAsync.txt");
+        }
+    }
 
 }
